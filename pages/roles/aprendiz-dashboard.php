180f517cce9985367ccc4320a68bec08c61362fa
daviardev <jerson.silva99@hotmail.com>
2023-08-21 19:15:15 -0500

    Aprendiz layout

---
+++ pages/roles/aprendiz-dashboard.php
@@ -0,0 +1,36 @@
+<?php
+    include './layouts/sidebar.php'
+?>
+
+<!DOCTYPE html>
+<html lang='es'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='shortcut icon' href='/public/favicon.jpeg' type='image/x-icon'>
+    <link rel='stylesheet' href='/css/globals.css'>
+    <link rel='stylesheet' href='/css/aprendiz-dashboard.css'>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9' crossorigin='anonymous'>
+    <title>Sistema · Aprendiz (Jerson David Silva Arjona)</title>
+</head>
+
+<body>
+<div class='wrapper'>
+    <header>
+        <nav class='navbar navbar-default navbar-static-top' style='background-color: #168a45;'>
+            <div class='header_logo divLogo'>
+                
+                <img
+                    style='padding-left: 30px; margin-bottom: 5px;'
+                    src='/public/logoSofia_noVersion.png'
+                />
+            </div>
+        </nav>
+    </header>
+</div>
+</body>
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js' integrity='sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm' crossorigin='anonymous'></script>
+<script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js' integrity='sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r' crossorigin='anonymous'></script>
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js' integrity='sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa' crossorigin='anonymous'></script>
+</html>
