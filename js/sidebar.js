587ef50c8d68e4e966bf0bd5b7d22d33ad9f3c55
daviardev <jerson.silva99@hotmail.com>
2023-08-21 19:14:27 -0500

    Sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,19 @@
+import { $ } from './dom.js'
+
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector('.toggle'),
+      modeSwitch = body.querySelector('.toggle-switch'),
+      modeText = body.querySelector('.mode-text')
+
+toggle.addEventListener('click' , () => {
+    sidebar.classList.toggle('close')
+})
+
+modeSwitch.addEventListener('click' , () => {
+    body.classList.toggle('dark')
+
+    body.classList.contains('dark')
+    ? modeText.innerHTML = 'Light mode'
+    : modeText.innerHTML = 'Dark mode'
+})
