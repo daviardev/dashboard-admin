2114e6cddd78688e15996cc00053706aba09bb5f
daviardev <jerson.silva99@hotmail.com>
2023-09-21 00:58:56 -0500

    Update database

--- db/dash-data.sql
+++ db/dash-data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-08-2023 a las 07:21:19
+-- Tiempo de generación: 21-09-2023 a las 07:57:46
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -24,6 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `aprendices`
+--
+
+CREATE TABLE `aprendices` (
+  `id` int(11) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `id_ficha` int(11) NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fichas`
+--
+
+CREATE TABLE `fichas` (
+  `id` int(11) NOT NULL,
+  `programa` int(11) NOT NULL,
+  `ficha` bigint(20) NOT NULL,
+  `alias` text NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `fichas`
+--
+
+INSERT INTO `fichas` (`id`, `programa`, `ficha`, `alias`, `estado`) VALUES
+(7, 11, 32, 'hgfhgfhfgjgfjghjhgkhg', 16),
+(8, 13, 6090989809090, 'asdsafsaf', 15);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `item`
 --
 
@@ -38,7 +73,31 @@ CREATE TABLE `item` (
 
 INSERT INTO `item` (`id`, `description`) VALUES
 (1, 'rol'),
-(2, 'documento');
+(2, 'documento'),
+(3, 'estado'),
+(5, 'estado_ficha');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programas`
+--
+
+CREATE TABLE `programas` (
+  `id` int(11) NOT NULL,
+  `nombre_programa` text NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `programas`
+--
+
+INSERT INTO `programas` (`id`, `nombre_programa`, `estado`) VALUES
+(10, 'fdsfdssdffds', 13),
+(11, 'hgfhgfh', 13),
+(12, 'gdfdsfdsdfsfdsdfs', 13),
+(13, 'nbvnbvnbvnvbnbvnbvnvbnvbnbvnbvnvb', 13);
 
 -- --------------------------------------------------------
 
@@ -63,7 +122,10 @@ CREATE TABLE `registropersonas` (
 --
 
 INSERT INTO `registropersonas` (`id`, `nombres`, `apellidos`, `tipo_doc`, `num_doc`, `correo`, `telefono`, `rol`, `contraseña`) VALUES
-(16, 'Jerson David', 'Silva Arjona', 5, 1045759468, 'dev.daviar@gmail.com', 300368312, 10, 'e698f2679be5ba5c9c0b0031cb5b057c');
+(40, 'daviar', 'dev', 5, 2, 'daviar@daviar.com', 32, 10, 'caf1a3dfb505ffed0d024130f58c5cfa'),
+(43, 'jh', 'as', 6, 6543, 'da@.qd.es', 423, 10, '1ff1de774005f8da13f42943881c655f'),
+(49, '123', '321', 7, 123321, '123@ds.das', 2147483647, 12, '9acacb906f59ed21c5c7e6bf78ea7323'),
+(50, 'jhsfhjkfsdhkfdskfdskhfdskhdskjsdkjsdkjsdkjkjsfdkjf', 'kjsdhkjsdjkhdskjfkjsdjkkjhfkdsjhfkjhdkjhsdkjhsdkjh', 5, 2147483647, 'jkhadskjsahkjdhsajksdkjdskjdhasd@hdjhasdkjhsakjdhk', 2147483647, 12, 'ba40bf59e82432e19b35232b4089be20');
 
 -- --------------------------------------------------------
 
@@ -89,17 +151,46 @@ INSERT INTO `sub_items` (`id`, `id_items`, `description`) VALUES
 (9, 2, 'Permiso por Protección Temporal'),
 (10, 1, 'Administrador'),
 (11, 1, 'Instructor'),
-(12, 1, 'Aprendiz');
+(12, 1, 'Aprendiz'),
+(13, 3, 'Activo'),
+(14, 3, 'Inactivo'),
+(15, 5, 'En formación'),
+(16, 5, 'Culminado');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `aprendices`
+--
+ALTER TABLE `aprendices`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `estado` (`estado`),
+  ADD KEY `id_ficha` (`id_ficha`),
+  ADD KEY `id_persona` (`id_persona`);
+
+--
+-- Indices de la tabla `fichas`
+--
+ALTER TABLE `fichas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `programa` (`programa`),
+  ADD KEY `estado` (`estado`),
+  ADD KEY `ficha` (`ficha`);
+
+--
 -- Indices de la tabla `item`
 --
 ALTER TABLE `item`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `programas`
+--
+ALTER TABLE `programas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `estado` (`estado`);
 
 --
 -- Indices de la tabla `registropersonas`
@@ -121,26 +212,64 @@ ALTER TABLE `sub_items`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `aprendices`
+--
+ALTER TABLE `aprendices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `fichas`
+--
+ALTER TABLE `fichas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT de la tabla `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `programas`
+--
+ALTER TABLE `programas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `registropersonas`
 --
 ALTER TABLE `registropersonas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT de la tabla `sub_items`
 --
 ALTER TABLE `sub_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `aprendices`
+--
+ALTER TABLE `aprendices`
+  ADD CONSTRAINT `aprendices_ibfk_1` FOREIGN KEY (`id_ficha`) REFERENCES `fichas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `aprendices_ibfk_2` FOREIGN KEY (`estado`) REFERENCES `sub_items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `aprendices_ibfk_3` FOREIGN KEY (`id`) REFERENCES `registropersonas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `fichas`
+--
+ALTER TABLE `fichas`
+  ADD CONSTRAINT `fichas_ibfk_1` FOREIGN KEY (`programa`) REFERENCES `programas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `programas`
+--
+ALTER TABLE `programas`
+  ADD CONSTRAINT `programas_ibfk_1` FOREIGN KEY (`estado`) REFERENCES `sub_items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `registropersonas`
