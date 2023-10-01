bf1366eb475780ea55b504906c8d48f65f469ba3
daviardev <jerson.silva99@hotmail.com>
2023-09-30 23:15:39 -0500

    Bettering login forma and upgrade alert error

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ session_start();
 include('./backend/conexion.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $numDoc = $_POST['num_doc'];
-    $pass = md5($_POST['contraseña']);
+    $numDoc = $_POST['numDocument'];
+    $passMD5 = md5($_POST['password']);
 
-    $select = "SELECT * FROM registropersonas WHERE num_doc = '$numDoc' && contraseña = '$pass'";
+    $select = "SELECT * FROM registropersonas WHERE num_doc = '$numDoc' AND contraseña = '$passMD5'";
 
     $query = mysqli_query($conn, $select);
 
@@ -21,9 +21,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['instructor_name'] = $row['nombres'];
             header('location:./instructor.php');
 
-        } else if ($row['rol'] == 12) {
-            $_SESSION['aprendiz_name'] = $row['nombres'];
-            header('location:./aprendiz.php');
         }
     } else {
         $error[] = "Datos incorrectos";
@@ -33,87 +30,71 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang='es'>
+
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <link rel='shortcut icon' href='./src/img/favicon.jpeg' type='image/x-icon'>
-    <title>Iniciar sesión · Centro de registros</title>
+    <link rel='shortcut icon' href='./src/img/favicon.png' type='image/x-icon'>
+    <title>Iniciar sesión · Gestion de registros</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel='stylesheet' href='./css/login.css'>
 </head>
+
 <body>
-    <div class='container-login'>
-        <section class='content-login'>
-            <form class='form-login' method='POST'>
-                <img
-                   src='./src/img/logoSofia.png'
-                   alt='Logo Sofia Plus'
-                />
+    <div class='container'>
+        <form method='POST' class='card'>
+            <span class='login'>
                 <?php
-                if (isset($error)) {
-                    foreach ($error as $error) {
-                        echo "
-                            <div class='error-txt'>"
-                            .$error.
-                            "</div>
+                    if (isset($error)) {
+                        foreach ($error as $error) {
+                            echo "
+                            <div class='alert alert-error'>
+                                <div class='icon__wrapper'>
+                                    <i class='bx bx-alarm-exclamation'></i>
+                                </div>
+                                <p>".$error."</p>   
+                            </div>
                         ";
+                        }
                     }
-                }
                 ?>
-                <h3 class='title-login'>ingreso usuarios registrados</h3>
-                <div class='input-content'>
-                    <input
-                      type='text'
-                      class='input'
-                      name='num_doc'
-                      placeholder='Número de Documento'
-                      required
-                    />
-                    <svg
-                        fill='currentColor'
-                        class='icon-input'
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='1em'
-                        height='1em'
-                        stroke='currentColor'
-                        viewBox='0 0 448 512'
-                        stroke-width='0'
-                    >
-                        <path
-                            d='M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z' 
-                        />
+                gestión de registros
+            </span>
+            <div class='inputBox'>
+                <input
+                    type='text'
+                    name='numDocument'
+                    required='required'
+                >
+                <span class='user'>
+                    <svg fill='currentColor' class='icon-input' xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' stroke='currentColor' viewBox='0 0 448 512' stroke-width='0'>
+                        <path d='M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z' />
                     </svg>
-                </div>
-                <div class='input-content'>
-                    <input
-                      type='password'
-                      class='input'
-                      name='contraseña'
-                      placeholder='Contraseña'
-                      required
-                    />
-                    <svg
-                        fill='currentColor'
-                        class='icon-input'
-                        width='1em'
-                        xmlns='http://www.w3.org/2000/svg'
-                        stroke='currentColor'
-                        height='1em'
-                        viewBox='0 0 24 24'
-                        stroke-width='0'
-                    >
-                    <path
-                        fill='none'
-                        d='M0 0h24v24H0V0z'>
-                    </path>
-                    <path d='M2 17h20v2H2v-2zm1.15-4.05L4 11.47l.85 1.48 1.3-.75-.85-1.48H7v-1.5H5.3l.85-1.47L4.85 7 4 8.47 3.15 7l-1.3.75.85 1.47H1v1.5h1.7l-.85 1.48 1.3.75zm6.7-.75l1.3.75.85-1.48.85 1.48 1.3-.75-.85-1.48H15v-1.5h-1.7l.85-1.47-1.3-.75L12 8.47 11.15 7l-1.3.75.85 1.47H9v1.5h1.7l-.85 1.48zM23 9.22h-1.7l.85-1.47-1.3-.75L20 8.47 19.15 7l-1.3.75.85 1.47H17v1.5h1.7l-.85 1.48 1.3.75.85-1.48.85 1.48 1.3-.75-.85-1.48H23v-1.5z'></path></svg>
-                </div>
-                <div class='content-button'>
-                    <button class='btn' name='submit' type='submit'>
-                        ingresar
-                    </button>
-                </div>
-            </form>
-        </section>
+                    Ingresar documento
+                </span>
+            </div>
+
+            <div class='inputBox'>
+                <input
+                    type='password'
+                    name='password'
+                    required="required"
+                >
+                <span>
+                    <svg fill='currentColor' class='icon-input' width='1em' xmlns='http://www.w3.org/2000/svg' stroke='currentColor' height='1em' viewBox='0 0 24 24' stroke-width='0'>
+                        <path fill='none' d='M0 0h24v24H0V0z'>
+                        </path>
+                        <path d='M2 17h20v2H2v-2zm1.15-4.05L4 11.47l.85 1.48 1.3-.75-.85-1.48H7v-1.5H5.3l.85-1.47L4.85 7 4 8.47 3.15 7l-1.3.75.85 1.47H1v1.5h1.7l-.85 1.48 1.3.75zm6.7-.75l1.3.75.85-1.48.85 1.48 1.3-.75-.85-1.48H15v-1.5h-1.7l.85-1.47-1.3-.75L12 8.47 11.15 7l-1.3.75.85 1.47H9v1.5h1.7l-.85 1.48zM23 9.22h-1.7l.85-1.47-1.3-.75L20 8.47 19.15 7l-1.3.75.85 1.47H17v1.5h1.7l-.85 1.48 1.3.75.85-1.48.85 1.48 1.3-.75-.85-1.48H23v-1.5z'>
+                        </path>
+                    </svg>
+                    Ingresar contraseña
+                </span>
+            </div>
+            <button class='enter' name='submit' type='submit'>
+                Ingresar
+            </button>
+        </form>
     </div>
 </body>
+
 </html>
